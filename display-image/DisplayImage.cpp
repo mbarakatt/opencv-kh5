71bf915c85705d2d95e9102f89431908c36cd2f6
Maxime Barakatt <mbarakatt@gmail.com>
2019-03-11 23:06:55 -0400

    minor

--- display-image/DisplayImage.cpp
+++ display-image/DisplayImage.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv )
 {
     if ( argc != 2 )
     {
-        printf("usage: DisplayImage.out <Image_Path>\n");
+        printf("usage: DisplayImage <Image_Path>\n");
         return -1;
     }
     Mat image;
